cca2b5516fd36c55651ca2c7aaf5c1ffc9d6c195
Matthew McDaniel <mmcdaniel@ara.com>
2018-08-29 10:08:24 -0600

    Implemented TimelineEntry as an abstract class.  Added TimelineAction and TimelineEvent .cpp files

--- projects/ui/src/widgets/TimelineEntries/TimelineAction.h
+++ projects/ui/src/widgets/TimelineEntries/TimelineAction.h
@@ -1,1 +1,34 @@
-#pragma once
+#ifndef BIOGEARSUI_WIDGETS_TIMELINE_ACTION_H
+#define BIOGEARSUI_WIDGETS_TIMELINE_ACTION_H
+
+//-------------------------------------------------------------------------------------------
+//- Copyright 2018 Applied Research Associates, Inc.
+//- Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+//- this file except in compliance with the License. You may obtain a copy of the License
+//- at:
+//- http://www.apache.org/licenses/LICENSE-2.0
+//- Unless required by applicable law or agreed to in writing, software distributed under
+//- the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+//- CONDITIONS OF ANY KIND, either express or implied. See the License for the
+//-  specific language governing permissions and limitations under the License.
+//-------------------------------------------------------------------------------------------
+
+//!
+//! \author Matt McDaniel
+//! \date   Aug 29 2018
+//!
+//! \brief Derived from Timeline Entry class, handles drawing of scenario actions
+
+#include "TimelineEntry.h"
+
+namespace biogears_ui {
+class TimelineAction : public TimelineEntry {
+public:
+  TimelineAction(QWidget* parent = 0);
+  ~TimelineAction();
+
+  void paintEvent(QPaintEvent* event) const;
+
+};
+}
+#endif
--- projects/ui/src/widgets/TimelineEntries/TimelineEntry.h
+++ projects/ui/src/widgets/TimelineEntries/TimelineEntry.h
@@ -1,1 +1,48 @@
-#pragma once
+#ifndef BIOGEARSUI_WIDGETS_TIMELINE_ENTRY_H
+#define BIOGEARSUI_WIDGETS_TIMELINE_ENTRY_H
+
+//-------------------------------------------------------------------------------------------
+//- Copyright 2018 Applied Research Associates, Inc.
+//- Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+//- this file except in compliance with the License. You may obtain a copy of the License
+//- at:
+//- http://www.apache.org/licenses/LICENSE-2.0
+//- Unless required by applicable law or agreed to in writing, software distributed under
+//- the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+//- CONDITIONS OF ANY KIND, either express or implied. See the License for the
+//-  specific language governing permissions and limitations under the License.
+//-------------------------------------------------------------------------------------------
+
+//!
+//! \author Matt McDaniel
+//! \date   Aug 29 2018
+//!
+//! \brief Base class for elements that are drawn on to timeline (scenario actions and patient events)
+
+//External Includes
+#include <QWidget>
+#include <QtGui>
+//Project Includes
+#include <biogears/framework/unique_propagate_const.h>
+
+namespace biogears_ui {
+class TimelineEntry : public QWidget {
+  Q_OBJECT
+public:
+  virtual void paintEvent(QPaintEvent* event) const = 0;
+
+  int getX() const { return _x; };
+  int getY() const { return _y; };
+  int getWidth() const { return _width; };
+  int getHeight() const {return _height; };
+  void setX(int x) { _x = x; };
+  void setY(int y) { _y = y; };
+
+protected:
+  int _x;
+  int _y;
+  int _height;
+  int _width;
+};
+}
+#endif
