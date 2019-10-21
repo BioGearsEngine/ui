83b1c9710d070a817cadc846e64eebe76699d3b1
Steven A White <sawhite@ara.com>
2019-10-21 16:13:46 -0400

    Updated Scenario.cpp to compile

--- projects/ui/cpp/biogears/Scenario.cpp
+++ projects/ui/cpp/biogears/Scenario.cpp
@@ -1,14 +1,20 @@
 #include "Scenario.h"
+#include <biogears/cdm/properties/SEScalarTime.h>
 #include <biogears/engine/BioGearsPhysiologyEngine.h>
-//#include <biogears/framework/scmp/scmp_channel.tci.h>
-#include <chrono>
 
+#include <biogears/cdm/patient/SEPatient.h>
+#include <biogears/cdm/system/environment/SEEnvironment.h>
+#include <biogears/cdm/system/environment/SEEnvironmentalConditions.h>
+#include <biogears/container/concurrent_queue.tci.h>
+#include <biogears/framework/scmp/scmp_channel.tci.h>
+
+#include <chrono>
 namespace bio {
 Scenario::Scenario(QString name)
   : _thread()
   , _logger(name.toStdString() + ".log")
   , _engine(biogears::CreateBioGearsEngine(&_logger))
-//, s_action_queue()
+  , _action_queue()
 {
 }
 //-------------------------------------------------------------------------------
@@ -23,7 +29,7 @@ void Scenario::run()
 {
   if (!_thread.joinable() && !_running) {
     _running = true;
-    //_thread = std::thread(&Scenario::physiology_thread_main, this);
+    _thread = std::thread(&Scenario::physiology_thread_main, this);
   }
 }
 //-------------------------------------------------------------------------------
@@ -41,12 +47,12 @@ void Scenario::step()
 //-------------------------------------------------------------------------------
 QString Scenario::patient_name()
 {
-  return "";
+  return _engine->GetPatient().GetName_cStr();
 }
 //-------------------------------------------------------------------------------
 QString Scenario::environment_name()
 {
-  return "";
+  return _engine->GetEnvironment()->GetName_cStr();
 }
 //-------------------------------------------------------------------------------
 QString Scenario::config_file()
@@ -69,53 +75,63 @@ Scenario& Scenario::config_file(QString&)
   return *this;
 }
 //-------------------------------------------------------------------------------
-State Scenario::get_physiology_state() &
-{
-  return {};
-}
-//-------------------------------------------------------------------------------
-Metrics Scenario::get_pysiology_metrics() &
-{
-  return {};
-}
-//-------------------------------------------------------------------------------
 std::function<void(void)> Scenario::step_as_func()
 {
   return std::function<void(void)>([this]() {
     if (this->_running) {
       return;
     } else {
-      //this->physiology_thread_step();
+      this->physiology_thread_step();
     }
     return;
   });
 }
-//biogears::scmp::Source<biogears::SEAction> Scenario::get_channel()
-//{
-//  return biogears::scmp::Channel<biogears::SEAction>().as_source();
-//}
-////-------------------------------------------------------------------------------
-//void Scenario::physiology_thread_main()
-//{
-//  using namespace std::chrono_literals;
-//
-//  //auto current_time = std::chrono::steady_clock::now();
-//  //auto prev = current_time;
-//  //while (_running) {
-//  //  physiology_thread_main();
-//  //  if (_throttle) {
-//  //    while (current_time - prev > 1_s) {
-//  //      std::this_thread::sleep_for(16_ms);
-//  //      current_time = std::chrono::steady_clock::now();
-//  //    }
-//  //  }
-//  }
-//}
-////-------------------------------------------------------------------------------
-//inline void Scenario::physiology_thread_step()
-//{
-//  //using namespace std::chrono_literals;
-//  //_engine->AdvanceModelTime(1_s);
-//}
-//-------------------------------------------------------------------------------s
+auto Scenario::get_channel() -> Source
+{
+  return Channel().as_source();
+}
+//-------------------------------------------------------------------------------
+void Scenario::physiology_thread_main()
+{
+  using namespace std::chrono_literals;
+
+  auto current_time = std::chrono::steady_clock::now();
+  auto prev = current_time;
+  while (_running) {
+    physiology_thread_main();
+    if (_throttle) {
+      while ((current_time - prev) > 1s) {
+        std::this_thread::sleep_for(16ms);
+        current_time = std::chrono::steady_clock::now();
+      }
+    }
+  }
+}
+//-------------------------------------------------------------------------------
+inline void Scenario::physiology_thread_step()
+{
+  using namespace std::chrono_literals;
+  _engine->AdvanceModelTime(1, biogears::TimeUnit::s);
+}
+//---------------------------------------------------------------------------------
+State Scenario::get_physiology_state()
+{
+  return {};
+}
+//---------------------------------------------------------------------------------
+Metrics Scenario::get_pysiology_metrics()
+{
+  return {};
+}
+//---------------------------------------------------------------------------------
+Conditions Scenario::get_pysiology_conditions()
+{
+  return {};
+}
+//---------------------------------------------------------------------------------
+double Scenario::get_simulation_time()
+{
+  return 0.;
+}
+//---------------------------------------------------------------------------------
 } //namspace ui
--- projects/ui/cpp/biogears/Scenario.h
+++ projects/ui/cpp/biogears/Scenario.h
@@ -2,40 +2,63 @@
 #include <functional>
 
 #include <QString>
+#include <QtQuick>
 
+#include <biogears/cdm/scenario/SEAction.h>
+#include <biogears/container/concurrent_queue.h>
 #include <biogears/engine/BioGearsPhysiologyEngine.h>
+#include <biogears/framework/scmp/scmp_channel.h>
+#include <biogears/threading/runnable.h>
 #include <biogears/threading/steppable.h>
-#include <biogears/threading/runnable.h>  
-//#include <biogears/framework/scmp/scmp_channel.h>
 namespace bio {
-struct State
-{
+struct State {
+  Q_GADGET
   bool alive;
   bool tacycardia;
+
+  Q_PROPERTY(bool alive MEMBER alive)
+  Q_PROPERTY(bool tacycardia MEMBER tacycardia)
 };
 struct Conditions {
+  Q_GADGET
   bool diabieties;
+  Q_PROPERTY(bool diabieties MEMBER diabieties)
 };
 struct Metrics {
+  Q_GADGET
   double respretory_rate_bpm;
   double heart_rate_bpm;
+  Q_PROPERTY(double respretory_rate_bpm MEMBER respretory_rate_bpm)
+  Q_PROPERTY(double heart_rate_bpm MEMBER heart_rate_bpm)
 };
 
-class Scenario : public biogears::Runnable, public biogears::Steppable<void(void)> {
+class Scenario : public QObject, public biogears::Runnable, public biogears::Steppable<void(void)> {
+  Q_OBJECT
+  Q_PROPERTY(State state READ get_physiology_state NOTIFY stateChanged)
+  Q_PROPERTY(Metrics metrics READ get_pysiology_metrics NOTIFY metricsChanged)
+  Q_PROPERTY(Conditions conditions READ get_pysiology_conditions NOTIFY conditionsChanged)
+  Q_PROPERTY(double time READ get_simulation_time NOTIFY timeAdvance)
+
 public:
   Scenario(QString name);
   ~Scenario();
 
+  using ActionQueue = biogears::ConcurrentQueue<std::queue<biogears::SEAction>>;
+  using Channel = biogears::scmp::Channel<ActionQueue>;
+  using Source = biogears::scmp::Source<ActionQueue>;
+
   QString patient_name();
   QString environment_name();
   QString config_file();
-  
+
   Scenario& patinet_name(QString&);
   Scenario& environment_name(QString&);
   Scenario& config_file(QString&);
-  
-  State get_physiology_state() &;
-  Metrics get_pysiology_metrics() &;
+
+  State get_physiology_state();
+  Metrics get_pysiology_metrics();
+  Conditions get_pysiology_conditions();
+  double get_simulation_time();
 
   void run() final;
   void stop() final;
@@ -43,19 +66,25 @@ public:
   void step() final;
   std::function<void(void)> step_as_func() final;
 
-  //biogears::scmp::Source<biogears::SEAction> get_channel();
+  Source get_channel();
+
+signals:
+  void stateChanged();
+  void metricsChanged();
+  void conditionsChanged();
+  void timeAdvance();
 
 protected:
-  //void physiology_thread_main();
-  //void physiology_thread_step();
+  void physiology_thread_main();
+  void physiology_thread_step();
+
 private:
   std::thread _thread;
   biogears::Logger _logger;
   std::unique_ptr<biogears::PhysiologyEngine> _engine;
-  //biogears::scmp::Channel<biogears::SEAction> _action_queue;
+  Channel _action_queue;
 
   std::atomic<bool> _running = false;
   std::atomic<bool> _throttle = true;
-
 };
 }
